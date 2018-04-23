b614e334c9e014041e5989499f376e192ff0474a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+using System.ComponentModel;
+using DevExpress.Xpf.Docking;
+
+using DevExpress.Xpf.Core.Serialization;
+using DevExpress.Mvvm.UI.Interactivity;
+using DevExpress.Utils.Serializing;
+
+namespace Q557494
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DXSerializer.AddLayoutUpgradeHandler(DockManager, layoutUpgrade);
+        }
+        private void layoutUpgrade(object sender, LayoutUpgradeEventArgs e)
+        {
+            if (e.RestoredVersion == "1")
+            {
+                documentGroup1.MDIStyle = MDIStyle.MDI;
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DockManager.SaveLayoutToXml("..//..//layout.xml");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            DockManager.RestoreLayoutFromXml("..//..//layout.xml");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string layoutVersion = DXSerializer.GetLayoutVersion(DockManager);
+            int number = int.Parse(layoutVersion);
+            number++;
+            DXSerializer.SetLayoutVersion(DockManager, number.ToString());
+        }
+    }
+
+}
+
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,56 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Forms
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports System.Xml.Serialization
+Imports System.ComponentModel
+Imports DevExpress.Xpf.Docking
+
+Imports DevExpress.Xpf.Core.Serialization
+Imports DevExpress.Mvvm.UI.Interactivity
+Imports DevExpress.Utils.Serializing
+
+Namespace Q557494
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+            DXSerializer.AddLayoutUpgradeHandler(DockManager, AddressOf layoutUpgrade)
+        End Sub
+        Private Sub layoutUpgrade(ByVal sender As Object, ByVal e As LayoutUpgradeEventArgs)
+            If e.RestoredVersion = "1" Then
+                documentGroup1.MDIStyle = MDIStyle.MDI
+            End If
+        End Sub
+        Private Sub Button_Click_1(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            DockManager.SaveLayoutToXml("..//..//layout.xml")
+        End Sub
+
+        Private Sub Button_Click_2(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            DockManager.RestoreLayoutFromXml("..//..//layout.xml")
+        End Sub
+
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim layoutVersion As String = DXSerializer.GetLayoutVersion(DockManager)
+            Dim number As Integer = Integer.Parse(layoutVersion)
+            number += 1
+            DXSerializer.SetLayoutVersion(DockManager, number.ToString())
+        End Sub
+    End Class
+
+End Namespace
+
